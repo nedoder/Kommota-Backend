b8fe62de26c4f1639aaef457c6fbb0791d89aefd
nedoder <nedoder89@gmail.com>
2021-03-02 00:08:55 +0100

    cors

--- app.js
+++ app.js
@@ -19,13 +19,14 @@ const checkout = require("./controllers/checkout");
 require('dotenv').config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.use(cors({
-    'allowedHeaders': ['sessionId', 'Content-Type'],
-    'exposedHeaders': ['sessionId'],
-    'origin': '*',
-    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    'preflightContinue': false
-}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Expose-Headers", "X-My-Custom-Header, X-Another-Custom-Header");
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+app.use(cors());
 app.use(upload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
