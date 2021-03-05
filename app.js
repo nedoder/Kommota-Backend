c2a49f3279abcdc7e08e26fbd276717dc557e698
nedoder <nedoder89@gmail.com>
2021-03-05 11:51:55 +0100

    cors

--- app.js
+++ app.js
@@ -19,11 +19,8 @@ const checkout = require("./controllers/checkout");
 require('dotenv').config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-app.use(cors());
-
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
