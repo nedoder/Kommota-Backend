711ff447ed71757cff0858e015f6a604a1085ae6
nedoder <nedoder89@gmail.com>
2021-03-05 11:11:07 +0100

    return

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const checkout = require("./controllers/checkout");
 require('dotenv').config();
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-// app.use(cors());
+app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
